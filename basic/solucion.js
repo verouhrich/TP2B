bf206708025d8fcb9027692a1a2848e8cb87cbde
Pablo Fernandez <ry08242@grupo.ypf.com>
2020-04-02 22:04:15 -0300

    resolucion hackerrank

--- basic/solucion.js
+++ basic/solucion.js
@@ -1,3 +1,36 @@
 let str = require('./data.js');
 
-console.log(str);
+function getTotalSeg(tipoVideo, str){
+
+    return str
+        .replace('<ul>','')
+        .replace('</ul>','')
+        .split('</li>')
+        .slice(0,-1)
+        .map(video => (
+            {
+                duracion: video
+                            .split('>')[0]
+                            .split('=')[1]
+                            .split('"')
+                            .join('')
+                            .split(':')
+                ,
+                tipo: video.split('>')[1]
+            }
+        ))
+        .map(video => (
+            {
+                duracion: parseInt(video.duracion[1]) + parseInt(video.duracion[0])*60,
+                tipo: video.tipo
+            }
+        ))
+        .filter(video => video.tipo == tipoVideo)
+        .reduce((total, actual) => (
+            {
+                duracion: total.duracion + actual.duracion
+            }
+        ))
+        .duracion;
+}
+console.log(getTotalSeg('Redux Video', str));
